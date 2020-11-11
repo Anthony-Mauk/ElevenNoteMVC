610e996571a506eaf422f1cd8a51e31d62087e58
Anthony-Mauk <anthonymauk.2020@gmail.com>
2020-11-11 11:43:36 -0500

    added category create

---
+++ ElevenNote.Models/CategoryCreate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models
+{
+    public class CategoryCreate
+    {
+        [Required]
+        [MaxLength(100,ErrorMessage ="Too many characters.")]
+        public string CategoryName { get; set; }
+    }
+}
